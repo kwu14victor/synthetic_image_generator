4ae1db298f41b68f956f57168794cb37a3f5f27e
kwu14victor <121959337+kwu14victor@users.noreply.github.com>
2024-11-12 11:22:15 -0600

    Update exemplar.py

--- exemplar.py
+++ exemplar.py
@@ -1,19 +1,25 @@
+'''
+11.12.2024 ver 0.0.0 by KLW
+This script aims demonstrate how to create synthetic cell images to train segmentation models
+The project is originated from the data scientist interview for St-Pierre Lab at BCM
+'''
 from main import *
 
-canvas = SynCellImage(120,120)
-for _ in range(10):
+canvas = SynCellImage(120,120) #create the synthetic data object
+for _ in range(10): #add ten random cells
     canvas.add_cells(SynCell((np.random.randint(10,110), np.random.randint(40,110)),\
                                  np.random.randint(20000,60000), np.random.randint(10,30),\
                                  np.random.uniform(1,2), np.random.randint(0,359)))
+#visualize the cell image
 plt.figure()
 plt.imshow(canvas.image)#, cmap='afmhot')
 plt.colorbar()
-plt.savefig('cellimage.png')
-
+plt.show()
+#visualize the cell label
 plt.figure()
 plt.imshow(canvas.label, cmap='viridis')
 plt.colorbar()
-plt.savefig('celllabel.png')
+plt.show()
 
 
 io.imsave('cellimage_original.png', canvas.image)
